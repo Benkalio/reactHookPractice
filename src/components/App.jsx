5cbee3b77df912fab4411cb7c1850a6ed4822f4f
Tennyson Ben-kalio <69404440+Benkalio@users.noreply.github.com>
2021-11-08 22:47:40 +0100

    setInterval

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
   let time = new Date().toLocaleTimeString();
+
   function refreshPage() {
     window.location.reload();
   }
-
+  setInterval(refreshPage, 1000);
   return (
     <div className="container">
       <h1>{time}</h1>
