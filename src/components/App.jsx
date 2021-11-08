21e941b071c9721c2fe67ea65aee0cecf14cc1b5
Tennyson Ben-kalio <69404440+Benkalio@users.noreply.github.com>
2021-11-08 22:30:07 +0100

    new window event

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 function App() {
   let time = new Date().toLocaleTimeString();
+  function refreshPage() {
+    window.location.reload();
+  }
 
   return (
     <div className="container">
