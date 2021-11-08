fa59dfcb29e734865148f0223182365b1291146b
Tennyson Ben-kalio <69404440+Benkalio@users.noreply.github.com>
2021-11-08 22:31:18 +0100

    refresh page function Implemented reload method in function to refresh page whenever getTime button is clicked

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="container">
       <h1>{time}</h1>
-      <button>Get Time</button>
+      <button onClick={refreshPage}>Get Time</button>
     </div>
   );
 }
